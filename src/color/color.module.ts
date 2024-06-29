32419c4daeada76920c0bdc1b957def13a8070f8
carlosxmerca <eduarmercado4@gmail.com>
2024-06-28 23:15:46 -0600

    :goal_net: add: request validations in create pet process

--- src/color/color.module.ts
+++ src/color/color.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ColorService } from './color.service';
 import { ColorController } from './color.controller';
 
+@Global()
 @Module({
   providers: [ColorService],
-  controllers: [ColorController]
+  controllers: [ColorController],
+  exports: [ColorService]
 })
 export class ColorModule {}
