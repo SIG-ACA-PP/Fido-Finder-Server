fc49b86d188b6e33639023c851797b8b7d022df5
Victor <00143320@uca.edu.sv>
2024-06-29 20:45:53 -0600

    feature: add MailerModule on notification module :construction:

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+
 
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.office365.com', //Outlook SMTP server
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'fidofinder@outlook.com',
+          pass: 'M3smer753357',
+        },
+      },
+      defaults: {
+        from: 'Fido Finder Team <fidofinder@outlook.com>'
+      }
+
+    })
+  ],
   providers: [NotificationsService],
   controllers: [NotificationsController]
 })
