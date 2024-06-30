cd4ee3b0cb50d6071b753f20f3b7fd7c02b9fe14
Victor <00143320@uca.edu.sv>
2024-06-29 21:32:59 -0600

    feature: add sendEmail logic in service :construction:

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { send } from 'process';
 import { Point } from 'src/models';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class NotificationsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private readonly mailerService: MailerService) {}
 
   // Obtain all users (id, email, phone) who are within
   // a radius (to be defined, for now use 2km) of
@@ -23,6 +25,37 @@ export class NotificationsService {
   // where a pet is reported lost (post.lost_in).
   notifyNearUsersByCommunity(point: Point) {}
 
-  private sendEmail() {}0
+  private async sendEmail(message : string, userId: string) {
+    const user = await this.prisma.users.findUnique({
+      where:{
+        id:userId,
+      }
+    });
+
+    if(!user) throw new NotFoundException("User not found");
+
+    if(!message)
+      {
+        message = `Hello ${user.name} \n
+        
+        Welcome to Fido Finder, this is a test email \n
+
+        Our Regards \n
+        the Fido Finder Team
+        `;
+      }
+
+      try {
+        await this.mailerService.sendMail({
+          to: user.email,
+          subject: 'Test Email',
+          text:message,
+        }); 
+      } catch (error : any) {
+       console.log(error); 
+      }
+      
+
+  }
   private sendWaMessage() {}
 }
