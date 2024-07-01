9eb3c469d0e4eec89e7a5bc0dd601ad7956a9a4a
Daniel Vladimir Solis Marroquin <00209020@uca.edu.sv>
2024-07-01 03:26:12 -0600

    :arrow_up: chore: update prisma npm dependency to version 5.16.1 & Add notifyNearUsersByResidence notification service

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -6,11 +6,26 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class NotificationsService {
   constructor(private prisma: PrismaService) {}
 
-  // Obtain all users (id, email, phone) who are within
-  // a radius (to be defined, for now use 2km) of
-  // the point where a pet is reported lost (post.lost_in)
-  // using their residence.
-  notifyNearUsersByResidence(point: Point) {}
+  // Obtener todos los usuarios (id, correo, teléfono) que se encuentren
+  // en un radio (2 km) del punto donde se reporta una mascota perdida (post.lost_in)
+  // usando su residencia.
+  async notifyNearUsersByResidence(point: Point, postId: string) {
+    const users = await this.prisma.$queryRaw`
+      SELECT u.id, u.email, u.phone_number
+      FROM users u
+      JOIN posts p 
+      ON ST_DWithin(
+          ST_Transform(u.residence, 3857), 
+          ST_Transform(ST_SetSRID(ST_MakePoint(${point.lon}, ${point.lat}), 4326), 3857), 
+          2000
+      )
+      WHERE p.is_lost = true
+      AND p.id = ${postId}::uuid;
+    `;
+
+    return users;
+  }
+
 
   // Obtain all users (id, email, phone) who are within
   // a radius (to be defined, for now use 2km) of
