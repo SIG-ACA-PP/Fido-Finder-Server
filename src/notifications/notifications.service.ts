4e7d1fda70501da4f34727d6e8be03ff4f7f70a8
Daniel Vladimir Solis Marroquin <00209020@uca.edu.sv>
2024-07-01 03:28:51 -0600

    :sparkles: feat: add notifyNearUsersByLocation notification service

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -6,9 +6,10 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class NotificationsService {
   constructor(private prisma: PrismaService) {}
 
-  // Obtener todos los usuarios (id, correo, teléfono) que se encuentren
-  // en un radio (2 km) del punto donde se reporta una mascota perdida (post.lost_in)
-  // usando su residencia.
+  // Obtain all users (id, email, phone) who are within
+  // a radius (to be defined, for now use 2km) of
+  // the point where a pet is reported lost (post.lost_in)
+  // using their residence.
   async notifyNearUsersByResidence(point: Point, postId: string) {
     const users = await this.prisma.$queryRaw`
       SELECT u.id, u.email, u.phone_number
@@ -31,7 +32,22 @@ export class NotificationsService {
   // a radius (to be defined, for now use 2km) of
   // the point where a pet is reported lost (post.lost_in)
   // using their current location.
-  notifyNearUsersByLocation(point: Point) {}
+  async notifyNearUsersByLocation(point: Point, postId: string) {
+    const users = await this.prisma.$queryRaw`
+      SELECT u.id, u.email, u.phone_number
+      FROM users u
+      JOIN posts p 
+      ON ST_DWithin(
+          ST_Transform(u.current_location, 3857), 
+          ST_Transform(ST_SetSRID(ST_MakePoint(${point.lon}, ${point.lat}), 4326), 3857), 
+          2000
+      )
+      WHERE p.is_lost = true
+      AND p.id = ${postId}::uuid;
+    `;
+
+    return users;
+  }
 
   // Obtain all users (id, email, phone) whose residence points
   // are in a neighborhood that also matches the point
