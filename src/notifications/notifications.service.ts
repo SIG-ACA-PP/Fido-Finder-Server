4c506ba6b4e4f8bede47f93ab6935efae5551028
carlosxmerca <eduarmercado4@gmail.com>
2024-07-01 22:13:17 -0600

    :wrench: update: notification services to use dtos

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,58 +1,52 @@
 import { Injectable } from '@nestjs/common';
 import { Point } from 'src/models';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { NotifyUserDto } from './dto';
 
 @Injectable()
 export class NotificationsService {
   constructor(private prisma: PrismaService) {}
 
-  // Obtain all users (id, email, phone) who are within
+  // Obtain all users (id, name, lastname, email, phone) who are within
   // a radius (to be defined, for now use 2km) of
   // the point where a pet is reported lost (post.lost_in)
   // using their residence.
-  async notifyNearUsersByResidence(point: Point, postId: string) {
+  async notifyNearUsersByResidence(point: Point): Promise<NotifyUserDto[]> {
     const users = await this.prisma.$queryRaw`
-      SELECT u.id, u.email, u.phone_number
+      SELECT u.id, u.name, u.lastname, u.email, u.phone_number
       FROM users u
-      JOIN posts p 
-      ON ST_DWithin(
+      WHERE ST_DWithin(
           ST_Transform(u.residence, 3857), 
           ST_Transform(ST_SetSRID(ST_MakePoint(${point.lon}, ${point.lat}), 4326), 3857), 
           2000
-      )
-      WHERE p.is_lost = true
-      AND p.id = ${postId}::uuid;
+      );
     `;
 
-    return users;
+    return users as NotifyUserDto[];
   }
 
-
-  // Obtain all users (id, email, phone) who are within
+  // Obtain all users (id, name, lastname, email, phone) who are within
   // a radius (to be defined, for now use 2km) of
   // the point where a pet is reported lost (post.lost_in)
   // using their current location.
-  async notifyNearUsersByLocation(point: Point, postId: string) {
+  async notifyNearUsersByLocation(point: Point): Promise<NotifyUserDto[]> {
     const users = await this.prisma.$queryRaw`
-      SELECT u.id, u.email, u.phone_number
+      SELECT u.id, u.name, u.lastname, u.email, u.phone_number
       FROM users u
-      JOIN posts p 
-      ON ST_DWithin(
+      WHERE ST_DWithin(
           ST_Transform(u.current_location, 3857), 
           ST_Transform(ST_SetSRID(ST_MakePoint(${point.lon}, ${point.lat}), 4326), 3857), 
           2000
-      )
-      WHERE p.is_lost = true
-      AND p.id = ${postId}::uuid;
+      );
     `;
 
-    return users;
+    return users as NotifyUserDto[];
   }
 
-  // Obtain all users (id, email, phone) whose residence points
+  // Obtain all users (id, name, lastname, email, phone) whose residence points
   // are in a neighborhood that also matches the point
   // where a pet is reported lost (post.lost_in).
-  async notifyNearUsersByCommunity(point: Point, postId: string) {
+  async notifyNearUsersByCommunity(postId: string): Promise<NotifyUserDto[]> {
     const users = await this.prisma.$queryRaw`
       WITH lost_community AS (
           SELECT colonia
@@ -62,13 +56,13 @@ export class NotificationsService {
           AND p.is_lost = true
           LIMIT 1
       )
-      SELECT u.id, u.email, u.phone_number
+      SELECT u.id, u.name, u.lastname, u.email, u.phone_number
       FROM users u
       JOIN communities c ON ST_Within(u.residence, c.geom)
       WHERE c.colonia = (SELECT colonia FROM lost_community);
     `;
 
-    return users;
+    return users as NotifyUserDto[];
   }
 
   private sendEmail() {}
