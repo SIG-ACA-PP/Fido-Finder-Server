6fff5204ef6919a4de51c10cf5c7aa4dbb381399
Victor <00143320@uca.edu.sv>
2024-07-05 23:03:22 -0600

    docs: add notifications docs for endpoints with examples :memo:

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Query } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { Point } from 'src/models';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiInternalServerErrorResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 // NOTE: This controller is just for tests
 @ApiTags('Notifications')
@@ -9,16 +9,67 @@ import { ApiTags } from '@nestjs/swagger';
 export class NotificationsController {
   constructor(private notificationsService: NotificationsService) { }
 
+  /**
+   * Sends a notification by email about a lost dog on the specify residence
+   * @param dto 
+   * @returns 
+   */
+  @ApiBody({
+    description: "The GeoJson coordinates of the residence",
+    type: Point
+  })
+  @ApiOkResponse({
+    description: "Email sent to users",
+    type: null
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Email could not been sent",
+    type: Error
+  })
   @Get('/users/residence')
   notifyNearUsersByResidence(@Body() dto: Point) {
     return this.notificationsService.notifyNearUsersByResidence(dto);
   }
 
+  /**
+   * Sends a notification by email about a lost dog on the specify location
+   * @param dto 
+   * @returns 
+   */
+  @ApiBody({
+    description: "The GeoJson coordinates of the location",
+    type: Point
+  })
+  @ApiOkResponse({
+    description: "Email sent to users",
+    type: null
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Email could not been sent",
+    type: Error
+  })
   @Get('/users/location')
   notifyNearUsersByLocation(@Body() dto: Point) {
     return this.notificationsService.notifyNearUsersByLocation(dto);
   }
 
+  /**
+   * Sends a notification by email about a lost dog on the specify community
+   * @param dto 
+   * @returns 
+   */
+  @ApiBody({
+    description: "The GeoJson coordinates of the community",
+    type: Point
+  })
+  @ApiOkResponse({
+    description: "Email sent to users",
+    type: null
+  })
+  @ApiInternalServerErrorResponse({
+    description: "Email could not been sent",
+    type: Error
+  })
   @Get('/users/community')
   notifyNearUsersByCommunity(@Query('id') postId: string) {
     return this.notificationsService.notifyNearUsersByCommunity(postId);
